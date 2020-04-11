c38c369ac2ec47e653436aab343eb123231840eb
walchko <walchko@users.noreply.github.com>
2020-04-10 21:13:59 -0400

    up

---
+++ slurm/files.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2014 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from subprocess import check_output  # call command line
+import os              # make directories, change current dir, etc
+import platform        # macOS or windows
+import shutil          # move and delete files/folders
+
+def run(cmd):
+    """Runs a command string and returns the output"""
+    cmds = cmd.split()
+    return check_output(cmds)
+
+
+def mkdir(path):
+    """Makes a directory"""
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        # folder was already created ... it's ok
+        pass
+
+
+def rmdir(path):
+    """Removes a directory"""
+    try:
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        # folder was already deleted or doesn't exist ... it's ok
+        pass
